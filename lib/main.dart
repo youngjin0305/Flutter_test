c010aa98eac84c90c44cbb8b220bb825ce940c27
youngjin0305 <blueati35@naver.com>
2023-06-07 15:41:33 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -9,9 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home : Text('test')
-    );
+    return const MaterialApp(home: Text('test'));
   }
 }
-
